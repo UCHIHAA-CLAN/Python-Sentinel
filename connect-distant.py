5fc78c8c61788cca6edba9ff8414468430f17d3e
yassinezorgui <zorguimohamedyassine@gmail.com>
2025-03-16 07:37:02 +0100

    Set up the project structure

--- connect-distant.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-username = input("Enter the username: ")
-ip_address = input("Enter the IP address: ")
-
-ssh_çommand = f"ssh {username}@{ip_address}"
-print(subprocess.run(ssh_çommand, shell=True, capture_output=True).stdout.decode())
